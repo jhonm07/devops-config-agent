23684a70029df4d40069cad408704bd7f1574fae
john Muñoz <jhonavilam@gmail.com>
2023-12-07 05:54:05 -0500

    settings var

--- script-confing-agent.sh
+++ script-confing-agent.sh
@@ -4,6 +4,6 @@ mkdir /home/azureuser/myagent/
 cd /home/azureuser/myagent/
  tar zxvf ../vsts-agent-linux-x64-3.230.0.tar.gz
 export agentname=$(hostname)
-./config.sh --unattended --url https://dev.azure.com/alilteam --auth pat --token 7hyu43qu5qqvttylrds54knavtlnwu24pukc6u3c4vqy3u7wgdoa --pool devops-scaleagent-casereview --agent $agentname --acceptTeeEula
- sudo ./svc.sh install
+./config.sh --unattended --url https://dev.azure.com/alilteam --auth pat --token aqjyyemzij2mpwu3axwq62qklkiy44uj5o3jdnipvgu4ckro45qq --pool devops-scaleagent-casereview --agent $agentname --acceptTeeEula 
+sudo ./svc.sh install
  sudo ./svc.sh start
