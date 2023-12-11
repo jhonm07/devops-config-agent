93705b9081ca6196ce0031ca941112cc0c0678b9
jhonm07 <jhonavilam@gmail.com>
2023-12-11 17:25:45 -0500

    Update config-agent-scale-set.sh

--- config-agent-scale-set.sh
+++ config-agent-scale-set.sh
@@ -3,6 +3,6 @@ mkdir /home/azureuser/myagent/
 cd /home/azureuser/myagent/
  tar zxvf ../vsts-agent-linux-x64-3.230.0.tar.gz
 export agentname=$(hostname)
-./config.sh --unattended --url https://dev.azure.com/alilteam --auth pat --token  pof7nzede2tkqzadp67it4sh7mlznj6pgqcmysfrptgxqo3wjgla --pool devops-scaleagent-casereview --agent $agentname --acceptTeeEula 
+./config.sh --unattended --url https://dev.azure.com/alilteam --auth pat --token  2nkczo54wll3fqhaxudqk4j7mkfkqi62z3k6adurnpl27dayicja --pool devops-scaleagent-casereview --agent $agentname --acceptTeeEula 
 sudo ./svc.sh install
  sudo ./svc.sh start >> /tmp/out.log
