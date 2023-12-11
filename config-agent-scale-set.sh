04671d6b891e320d871e2769a26d6fce6ac5740b
jhonm07 <jhonavilam@gmail.com>
2023-12-11 12:35:00 -0500

    Update config-agent-scale-set.sh

--- config-agent-scale-set.sh
+++ config-agent-scale-set.sh
@@ -9,9 +9,9 @@ patToken="6bf2wh2ecqwjyqhhsjrsavquker4zhnzizx5rfe4me6ny5obxzta"
 
 # Descarga el agente
 mkdir myagent && cd myagent
-wget https://vstsagentpackage.azureedge.net/agent/2.195.2/vsts-agent-linux-x64-2.195.2.tar.gz
-tar zxvf vsts-agent-linux-x64-2.195.2.tar.gz
-
+wget https://vstsagentpackage.azureedge.net/agent/3.230.2/vsts-agent-linux-x64-3.230.2.tar.gz
+tar zxvf vsts-agent-linux-x64-3.230.2.tar.gz
+./config.sh --unattended --url https://dev.azure.com/alilteam --auth pat --token  --pool devops-scaleagent-casereview --agent $agentname --acceptTeeEula
 
 
 # Instala el agente como servicio
