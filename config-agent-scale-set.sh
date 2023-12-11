31891d4834ef9ad2c169e00d5a5244ca5f402557
jhonm07 <jhonavilam@gmail.com>
2023-12-10 21:22:57 -0500

    Update config-agent-scale-set.sh

--- config-agent-scale-set.sh
+++ config-agent-scale-set.sh
@@ -1,6 +1,8 @@
-Y2QgL2hvbWUvYXp1cmV1c2VyL215YWdlbnQKZXhwb3J0IEFaUF9VUkw9aHR0cHM6Ly9kZXYuYXp1
-cmUuY29tL2FsaWx0ZWFtCmV4cG9ydCBBWlBfVE9LRU49N2h5dTQzcXU1cXF2dHR5bHJkczU0a25h
-dnRsbnd1MjRwdWtjNnUzYzR2cXkzdTd3Z2RvYQpleHBvcnQgQVpQX0FHRU5UUE9PTD1kZXZvcHMt
-c2NhbGVhZ2VudC1jYXNlcmV2aWV3CmV4cG9ydCBBWlBfQUdFTlRfTkFNRT1hZ2VudDAxCi4vY29u
-ZmlnLnNoIHJlbW92ZQpzdWRvICAuL3N2Yy5zaCB1bmluc3RhbGwKc3VkbyAuL3N2Yy5zaCBpbnN0
-YWxsCnN1ZG8gLi9zdmMuc2ggc3RhcnQK
+rm -rf  /home/azureuser/myagent/
+mkdir /home/azureuser/myagent/
+cd /home/azureuser/myagent/
+ tar zxvf ../vsts-agent-linux-x64-3.230.0.tar.gz
+export agentname=$(hostname)
+./config.sh --unattended --url https://dev.azure.com/alilteam --auth pat --token aqjyyemzij2mpwu3axwq62qklkiy44uj5o3jdnipvgu4ckro45qq --pool devops-scaleagent-casereview --agent $agentname --acceptTeeEula 
+sudo ./svc.sh install
+ sudo ./svc.sh start
