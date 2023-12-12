980fc8234030cce08c8355fc40da4d99544e5212
jhonm07 <jhonavilam@gmail.com>
2023-12-12 09:41:20 -0500

    Update config-agent-scale-set.sh

--- config-agent-scale-set.sh
+++ config-agent-scale-set.sh
@@ -5,7 +5,7 @@ ORGANIZATION_URL="https://dev.azure.com/alilteam"
 PROJECT_NAME="devops-alil-case-processor"
 POOL_NAME="devops-scaleagent-casereview"
 AGENT_NAME="Agent-$(hostname)"
-PAT_TOKEN="mhau3nb2uaqyubg2bejny6dlyhszgof2zdhz667fp6ry74vgfy3a"
+PAT_TOKEN="yngt4r7jjdt44ov3ku7fuwmbopntrsztycmgqx2hc3pmxovrgc2q"
 
 # Descargar e instalar el Azure Pipelines Agent
 curl -O https://vstsagentpackage.azureedge.net/agent/3.230.2/vsts-agent-linux-x64-3.230.2.tar.gz
